6525a49d3fadf6589b9271a7c0fc510c1f3efe40
nicole fell <nikkels.fell@gmail.com>
2020-09-28 17:55:41 -0700

    fixing image formatting issues

--- script.js
+++ script.js
@@ -25,8 +25,9 @@ let images = [
     "/Images/photoSlide/IMG_4624.jpeg",
     "/Images/photoSlide/IMG_5929.jpeg",
     "/Images/photoSlide/IMG_8256.jpeg",
-    "/Images/photoSlide/IMG_0119.jpeg"
+    "/Images/photoSlide/IMG_4902.jpeg"
 ]
+
 let captions = [
     "Choose godly wisdom over worldly wisdom",
     "I am confident I will see the Lord's goodness here in the land of the living",
@@ -36,7 +37,7 @@ let captions = [
     "But even the best years are filled with pain and trouble... Who can comprehend the power of your anger? Your wrath is as awesome as the fear you deserve. Teach us to realize the brevity of life, so that we may grow in wisdom",
     "The tounge is a small thing that makes grand speeches",
     "For he will conceal me when troubles come; he will hide me in his sanctuary. He will place me out of reach on a high rock. Then I will hold my head high, above my enemies who surround me. At his sanctuary I will offer sacrafices with shouts of joy, singing and praising the Lord with music",
-    ""
+    "Make every effort to add to your faith goodness; and to goodness, knowledge; and to knowledge, self control; and to self control, perseverance; and to perseverance, godliness; and to godliness, mutual affection; and to mutual affection, love"
 ]
 let authors = [
     "",
@@ -47,7 +48,7 @@ let authors = [
     "Psalms 90:10-12",
     "James 3:5",
     "Psalms 27:5-6",
-    ""
+    "2 Peter 1:5-6"
 ]
 
 //Move pictures when left/right arrows are pressed
